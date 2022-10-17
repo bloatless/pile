6f04cdf3f66a8f3a1b36580b9e7b94454c1699e8
Simon Samtleben <s.samtleben@oak.team>
2022-10-17 15:48:19 +0200

    Switch to sqlite

--- config/config.sample.php
+++ config/config.sample.php
@@ -2,41 +2,47 @@
 
 return [
     'logger' => [
-        // path to logs directory
+        'type' => 'file',
         'path_logs' => __DIR__ . '/../logs',
-
-        // min log level
-        'min_level' => 'warning',
+        'min_level' => 'notice',
     ],
 
     'db' => [
         // mysql db credentials
         'connections' => [
             'db1' => [
-                'driver' => 'mysql',
-                'host' => 'localhost',
-                'database' => 'pile',
-                'username' => '',
-                'password' => '',
-                'charset' => 'utf8', // Optional
-                'timezone' => 'Europe/Berlin', // Optional
+                'driver' => 'sqlite',
+                'database' => __DIR__ . '/../storage/database/sample.sqlite',
             ],
         ],
 
         'default_connection' => 'db1',
     ],
 
-    // path to view/layout files
-    'path_views' => __DIR__ . '/../resources/views',
+    'renderer' => [
+        'path_views' => __DIR__ . '/../resources/views',
+        'compile_path' => __DIR__ . '/../cache/compile',
+        'view_components' => [
+
+        ],
+    ],
 
     'auth' => [
         // valid api keys allowed to store logs
-        'api_keys' => [],
+        'api_keys' => [
+            '123123123',
+        ],
+
+        'backend' => 'array',
 
         // users allowed to view logs
         // HINT: passwords need to be generated using "password_hash" method
-        'users' => [
-            'foo' => '$2y$10$hJpespHOJUYzFtHIQk57OusBdwIOXz.8tUdbb9j545Meh2wmeshMm',
-        ],
+        'backends' => [
+            'array' => [
+                'users' => [
+                    'foo' => '$2y$10$hJpespHOJUYzFtHIQk57OusBdwIOXz.8tUdbb9j545Meh2wmeshMm',
+                ],
+            ]
+        ]
     ]
 ];
