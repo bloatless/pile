6a5ac162969c281bb8a601ef8594d053ac3304a3
Simon Samtleben <s.samtleben@oak.team>
2022-12-14 09:03:54 +0100

    Add sqlite db-structure

--- db_structure.sql
+++ db_structure.sql
@@ -1,12 +1,15 @@
-CREATE TABLE "logs" (
-    "log_id" integer not null primary key autoincrement,
-    "source" varchar(100) not null,
-    "message" text not null,
-    "level" integer NOT NULL default '400',
-    "level_name" varchar(50) NOT NULL DEFAULT 'error',
-    "context" text,
-    "channel" varchar(100) DEFAULT NULL,
-    "extra" text,
-    "created_at" datetime
-);
-CREATE INDEX logs_idx ON logs (log_id,source,level,created_at);
+CREATE TABLE `logs` (
+    `log_id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `source` varchar(100) NOT NULL,
+    `message` text NOT NULL,
+    `level` smallint(6) NOT NULL DEFAULT 400,
+    `level_name` varchar(50) NOT NULL DEFAULT 'error',
+    `context` text DEFAULT NULL,
+    `channel` varchar(100) DEFAULT NULL,
+    `extra` text DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`log_id`),
+    KEY `isld_idx` (`log_id`,`source`,`level`,`created_at`),
+    KEY `level_idx` (`level`,`level_name`) USING BTREE,
+    KEY `source_idx` (`source`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
